646e936033b4677a72bff12dfc789e1bf3185752
FranAguilarlt <79863830+FranAguilarlt@users.noreply.github.com>
2021-12-11 11:53:36 +0100

    Update main.py

--- P2_Francesc_Aguilar/main.py
+++ P2_Francesc_Aguilar/main.py
@@ -6,14 +6,22 @@ os.chdir('/')
 # ask seconds of cropage
 d = str(10)
 
+
+
+#cut the video for d seconds
 subprocess.call(
     ['ffmpeg', '-ss', '00:00:00', '-i', 'home/fran/Documents/test.mp4', '-to', '00:00:' + d, '-c', 'copy',
      'home/fran/Documents/output.mp4'])
 
+
+
+# create video with yuv spectogram
 subprocess.call(
     ['ffmpeg','-y','-i','home/fran/Documents/output.mp4','-vf', 'split=2[a][b],[b]histogram,format=yuva444p[hh],[a][hh]overlay',
      'home/fran/Documents/histo.mp4'])
 
+
+#creating rescaled videos
 subprocess.call(
     ['ffmpeg','-i','home/fran/Documents/output.mp4','-vf', 'scale=1280:720',
      'home/fran/Documents/1280x720.mp4'])
@@ -28,6 +36,8 @@ subprocess.call(
     ['ffmpeg','-i','home/fran/Documents/output.mp4','-vf', 'scale=160:120',
      'home/fran/Documents/160x120.mp4'])
 
+
+#changing codec and changing stereo to mono
 subprocess.call(
     ['ffmpeg','-i','home/fran/Documents/bagpipes.flac','-ac' ,'1',
      'home/fran/Documents/mono.mp3'])
